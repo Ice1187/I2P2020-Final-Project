c60460fa5e46eb562759c25b16d9a372d7081160
Alan <alan0988469026@gmail.com>
2020-06-27 16:30:05 +0800

    chore: add TODO

--- src/AITemplate/Porting.cpp
+++ src/AITemplate/Porting.cpp
@@ -1,10 +1,11 @@
 #include <UltraOOXX/Wrapper/Porting.h>
 // #include "AITemplate.h"
+// TODO: Do not edit this !!!
 #include "DummyAI.h"
 #define AI DummyAI
 
 // Do not edit this!
-void* getai()
+void *getai()
 {
     AIInterface *ptr = new AI();
     return ptr;
