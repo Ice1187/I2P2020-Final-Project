13e1148292557a8dfc05151d6b5f5b55acd9502e
Alan <alan0988469026@gmail.com>
2020-06-28 01:45:27 +0800

    feat: maintain wintag in Minmax_AI

--- src/AITemplate/MinMaxAI.h
+++ src/AITemplate/MinMaxAI.h
@@ -12,16 +12,174 @@
 class MinMaxAI : public AIInterface
 {
 private:
+    using Tag = TA::BoardInterface::Tag;
     int prev_x, prev_y;
     bool order;
     TA::UltraBoard all_board;
 
-    TA::BoardInterface::Tag player2tag(bool player)
+    Tag player2tag(bool player)
     {
         if (player == true)
-            return TA::BoardInterface::Tag::O;
+            return Tag::O;
         else
-            return TA::BoardInterface::Tag::X;
+            return Tag::X;
+    }
+
+    bool addWinTag(const std::pair<int, int> &pos, TA::BoardInterface &board)
+    {
+        if (board.getWinTag() != Tag::None)
+            return false;
+
+        const int &pos_x = pos.first;
+        const int &pos_y = pos.second;
+
+        // horizontal
+        if (board.state(pos_x, pos_y) == board.state((pos_x + 1) % 3, pos_y) &&
+            board.state(pos_x, pos_y) == board.state((pos_x + 2) % 3, pos_y))
+        {
+            board.setWinTag(board.state(pos_x, pos_y));
+            return true;
+        }
+
+        // vertical
+        if (board.state(pos_x, pos_y) == board.state(pos_x, (pos_y + 1) % 3) &&
+            board.state(pos_x, pos_y) == board.state(pos_x, (pos_y + 2) % 3))
+        {
+            board.setWinTag(board.state(pos_x, pos_y));
+            return true;
+        }
+
+        // diagonal
+        if (pos_x == pos_y &&
+            board.state(pos_x, pos_y) == board.state((pos_x + 1) % 3, (pos_y + 1) % 3) &&
+            board.state(pos_x, pos_y) == board.state((pos_x + 2) % 3, (pos_y + 2) % 3))
+        {
+            board.setWinTag(board.state(pos_x, pos_y));
+            return true;
+        }
+
+        // subdiagonal
+        if ((pos_x + pos_y) == 2 &&
+            board.state(pos_x, pos_y) == board.state((pos_x + 2) % 3, (pos_y + 1) % 3) &&
+            board.state(pos_x, pos_y) == board.state((pos_x + 1) % 3, (pos_y + 2) % 3))
+        {
+            board.setWinTag(board.state(pos_x, pos_y));
+            return true;
+        }
+
+        return false;
+    }
+    bool addSubboardWinTag(const std::pair<int, int> &pos, TA::Board &board)
+    {
+        if (addWinTag(pos, board))
+            return true;
+        if (board.full())
+            board.setWinTag(Tag::Tie);
+        return false;
+    }
+    bool addAllboardWinTag(const std::pair<int, int> &pos, TA::UltraBoard &board)
+    {
+        if (addWinTag(pos, board))
+            return true;
+        return false;
+    }
+
+    bool removeWinTag(TA::BoardInterface &board)
+    {
+        if (board.getWinTag() == Tag::None)
+            return false;
+        if (board.getWinTag() == Tag::Tie)
+        {
+            board.setWinTag(Tag::None);
+            return false;
+        }
+
+        // if win_lines >= 1,  wintag remain no change.
+        int win_lines = 0;
+
+        // horizontal
+        for (int i = 0; i < 3; i++)
+        {
+            if (board.state(i, 0) == Tag::O &&
+                board.state(i, 1) == Tag::O &&
+                board.state(i, 2) == Tag::O)
+                win_lines += 1;
+            if (board.state(i, 0) == Tag::X &&
+                board.state(i, 1) == Tag::X &&
+                board.state(i, 2) == Tag::X)
+                win_lines += 1;
+        }
+
+        // vertical
+        for (int j = 0; j < 3; j++)
+        {
+            if (board.state(0, j) == Tag::O &&
+                board.state(1, j) == Tag::O &&
+                board.state(2, j) == Tag::O)
+                win_lines += 1;
+            if (board.state(0, j) == Tag::X &&
+                board.state(1, j) == Tag::X &&
+                board.state(2, j) == Tag::X)
+                win_lines += 1;
+        }
+
+        // diagonal
+        if (board.state(0, 0) == Tag::O &&
+            board.state(1, 1) == Tag::O &&
+            board.state(2, 2) == Tag::O)
+            win_lines += 1;
+        if (board.state(0, 0) == Tag::X &&
+            board.state(1, 1) == Tag::X &&
+            board.state(2, 2) == Tag::X)
+            win_lines += 1;
+
+        // subdiagonal
+        if (board.state(0, 2) == Tag::O &&
+            board.state(1, 1) == Tag::O &&
+            board.state(2, 0) == Tag::O)
+            win_lines += 1;
+        if (board.state(0, 2) == Tag::X &&
+            board.state(1, 1) == Tag::X &&
+            board.state(2, 0) == Tag::X)
+            win_lines += 1;
+
+        if (win_lines == 0)
+        {
+            board.setWinTag(Tag::None);
+            return true;
+        }
+
+        return false;
+    }
+
+    void putChess(const std::pair<int, int> &pos, bool player)
+    {
+        if (this->all_board.get(pos.first, pos.second) != Tag::None)
+        {
+            std::cout << "minmax pos is not None\n";
+            throw "error";
+        }
+
+        this->all_board.get(pos.first, pos.second) = player2tag(player);
+        // add subboard wintag
+        if (addSubboardWinTag(std::make_pair(pos.first % 3, pos.second % 3), this->all_board.sub(pos.first / 3, pos.second / 3)))
+            // add allboard wintag
+            addAllboardWinTag(std::make_pair(pos.first / 3, pos.second / 3), this->all_board);
+    }
+
+    void removeChess(const std::pair<int, int> &pos, bool player)
+    {
+        if (this->all_board.get(pos.first, pos.second) != player2tag(player))
+        {
+            std::cout << "you don't own the chess, so you can't remove it\n";
+            throw "error";
+        }
+
+        // update subboard wintag
+        this->all_board.get(pos.first, pos.second) = Tag::None;
+        if (removeWinTag(this->all_board.sub(pos.first / 3, pos.second / 3)))
+            // add allboard wintag
+            removeWinTag(this->all_board);
     }
 
 public:
@@ -61,7 +219,7 @@ public:
                 cur_step.first = i + subboard_x * 3;
                 for (int j = 0; j < 3; j++)
                 {
-                    if (subboard.state(i, j) == TA::BoardInterface::Tag::None)
+                    if (subboard.state(i, j) == Tag::None)
                     {
                         cur_step.second = j + subboard_y * 3;
                         eval = minmax(cur_step, 3, -INF, INF, this->order);
@@ -90,7 +248,7 @@ public:
                         cur_step.first = i + subboard_x * 3;
                         for (int j = 0; j < 3; j++)
                         {
-                            if (subboard.state(i, j) == TA::BoardInterface::Tag::None)
+                            if (subboard.state(i, j) == Tag::None)
                             {
                                 cur_step.second = j + subboard_y * 3;
                                 eval = minmax(cur_step, 3, -INF, INF, this->order);
@@ -116,26 +274,15 @@ public:
     {
         if (depth == 0)
         {
-            if (this->all_board.get(pos.first, pos.second) == TA::BoardInterface::Tag::None)
-            {
-                this->all_board.get(pos.first, pos.second) = player2tag(player);
-                int ret = evalPos();
-                this->all_board.get(pos.first, pos.second) = TA::BoardInterface::Tag::None;
-                return ret;
-            }
-            std::cout << "minmax pos is not None\n";
-            throw "error";
+            putChess(pos, player);
+            int ret = evalPos();
+            removeChess(pos, player);
+            return ret;
         }
 
         if (player)
         {
-            if (this->all_board.get(pos.first, pos.second) == TA::BoardInterface::Tag::None)
-                this->all_board.get(pos.first, pos.second) = player2tag(player);
-            else
-            {
-                std::cout << "minmax pos is not None\n";
-                throw "error";
-            }
+            putChess(pos, player);
 
             int eval = 0;
             int maxEval = -INF;
@@ -151,18 +298,12 @@ public:
                     break;
             }
 
-            this->all_board.get(pos.first, pos.second) = TA::BoardInterface::Tag::None;
+            removeChess(pos, player);
             return maxEval;
         }
         else
         {
-            if (this->all_board.get(pos.first, pos.second) == TA::BoardInterface::Tag::None)
-                this->all_board.get(pos.first, pos.second) = player2tag(player);
-            else
-            {
-                std::cout << "minmax pos is not None\n";
-                throw "error";
-            }
+            putChess(pos, player);
 
             int eval = 0;
             int minEval = INF;
@@ -178,7 +319,7 @@ public:
                     break;
             }
 
-            this->all_board.get(pos.first, pos.second) = TA::BoardInterface::Tag::None;
+            removeChess(pos, player);
             return minEval;
         }
     }
@@ -199,7 +340,7 @@ public:
         {
             for (int i = 0; i < 3; i++)
                 for (int j = 0; j < 3; j++)
-                    if (subboard.state(i, j) == TA::BoardInterface::Tag::None)
+                    if (subboard.state(i, j) == Tag::None)
                         moves.emplace_back(std::make_pair(i + subboard_x * 3, j + subboard_y * 3));
         }
         else
@@ -210,7 +351,7 @@ public:
                     subboard = this->all_board.sub(subboard_x, subboard_y);
                     for (int i = 0; i < 3; i++)
                         for (int j = 0; j < 3; j++)
-                            if (subboard.state(i, j) == TA::BoardInterface::Tag::None)
+                            if (subboard.state(i, j) == Tag::None)
                                 moves.emplace_back(std::make_pair(i + subboard_x * 3, j + subboard_y * 3));
                 }
         }
