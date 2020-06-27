810239466f80d5efcd52b3ddce55997a5e5c38e8
Ice1187 <Icing1187@gmail.com>
2020-06-27 12:44:44 +0800

    fix: add override specifier

--- src/GUI/GUIInterface.h
+++ src/GUI/GUIInterface.h
@@ -21,12 +21,12 @@ namespace TA
     };
 
     using std::printf;
-    #define ESC "\033"
-    class ASCII: public GUIInterface
+#define ESC "\033"
+    class ASCII : public GUIInterface
     {
-        const static int GRAPH_HIGHT = 7+15;
+        const static int GRAPH_HIGHT = 7 + 15;
         const static int GRAPH_WIDTH = 80;
-        
+
         const static int TEXT_HIGHT = 10;
         const static int TEXT_WIDTH = 80;
 
@@ -35,12 +35,12 @@ namespace TA
 
         void cls()
         {
-            printf( ESC "[H" ESC "[J" );
+            printf(ESC "[H" ESC "[J");
         }
 
         void gotoxy(int y, int x)
         {
-            printf( ESC "\033[%d;%df", y, x);
+            printf(ESC "\033[%d;%df", y, x);
         }
 
         void updateTextBuf()
@@ -50,31 +50,31 @@ namespace TA
             std::string tmp, last;
 
             m_preparedText.clear();
-            for(int L=0 ; L < TEXT_HIGHT - 2; ++L)
+            for (int L = 0; L < TEXT_HIGHT - 2; ++L)
             {
-                if( last == "" )
+                if (last == "")
                     getline(ss, last);
 
-                tmp = last.substr(0, std::min((size_t)TEXT_WIDTH, last.size()) );
-    
-                if( tmp.size() == last.size() )
+                tmp = last.substr(0, std::min((size_t)TEXT_WIDTH, last.size()));
+
+                if (tmp.size() == last.size())
                     last = "";
-                else 
+                else
                     last = last.substr(TEXT_WIDTH);
-    
+
                 m_preparedText = tmp + "\n" + m_preparedText;
             }
             m_textbuf = m_textbuf.substr(0, TEXT_HIGHT * TEXT_WIDTH);
-            m_preparedText =  ban + "\n" + m_preparedText + ban;
+            m_preparedText = ban + "\n" + m_preparedText + ban;
         }
 
         void showText()
         {
-            gotoxy(GRAPH_HIGHT+1, 0);
-            printf( ESC "[J" );
-            gotoxy(GRAPH_HIGHT+1, 0);
+            gotoxy(GRAPH_HIGHT + 1, 0);
+            printf(ESC "[J");
+            gotoxy(GRAPH_HIGHT + 1, 0);
             puts(m_preparedText.c_str());
-            gotoxy(GRAPH_HIGHT+TEXT_HIGHT+1, 0);
+            gotoxy(GRAPH_HIGHT + TEXT_HIGHT + 1, 0);
             std::fflush(stdout);
         }
 
@@ -83,7 +83,7 @@ namespace TA
         {
             cls();
             puts(
-R"( _   _ _ _             _____  _______   ____   __
+                R"( _   _ _ _             _____  _______   ____   __
 | | | | | |           |  _  ||  _  \ \ / /\ \ / /
 | | | | | |_ _ __ __ _| | | || | | |\ V /  \ V /
 | | | | | __| '__/ _` | | | || | | |/   \  /   \
@@ -92,44 +92,50 @@ R"( _   _ _ _             _____  _______   ____   __
 )");
         }
 
-        virtual void appendText(std::string str)
+        virtual void appendText(std::string str) override
         {
             m_textbuf = str + m_textbuf;
             updateTextBuf();
             showText();
         }
 
-        int toPrintChar(BoardInterface::Tag t){
-            switch(t) {
-                case BoardInterface::Tag::O: return 'O';
-                case BoardInterface::Tag::X: return 'X';
-                default:
-                    return ' ';
+        int toPrintChar(BoardInterface::Tag t)
+        {
+            switch (t)
+            {
+            case BoardInterface::Tag::O:
+                return 'O';
+            case BoardInterface::Tag::X:
+                return 'X';
+            default:
+                return ' ';
             }
         }
 
-        virtual void updateGame(UltraBoard b)
+        virtual void updateGame(UltraBoard b) override
         {
-            gotoxy(7+1, 0);
+            gotoxy(7 + 1, 0);
             const std::string buf(20, ' ');
 
-            for (int i=0;i<9;++i)
+            for (int i = 0; i < 9; ++i)
             {
                 std::printf("%s", buf.c_str());
-                for (int j=0;j<9;++j)
+                for (int j = 0; j < 9; ++j)
                 {
                     std::putchar(toPrintChar(b.get(i, j)));
-                    if (j == 2 || j == 5) std::putchar('|');
+                    if (j == 2 || j == 5)
+                        std::putchar('|');
                 }
                 std::putchar('\n');
-                if (i==2 ||i==5) {
+                if (i == 2 || i == 5)
+                {
                     std::printf("%s", buf.c_str());
-                    std::puts(std::string(12,'-').c_str());
+                    std::puts(std::string(12, '-').c_str());
                 }
             }
 
-            gotoxy(GRAPH_HIGHT+TEXT_HIGHT+1, 0);
+            gotoxy(GRAPH_HIGHT + TEXT_HIGHT + 1, 0);
         }
     };
-    #undef ESC
-}
+#undef ESC
+} // namespace TA
