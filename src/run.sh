f5f840c66efb7e1a3fe24349761d81c896bddb87
Ice1187 <Icing1187@gmail.com>
2020-06-27 19:33:00 +0800

    fix: add run.sh Usage prompt

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,14 @@
 
 if [[ "$#" -ne 2 ]]; then
     echo 'Usage: ./run.sh <AI 1 type> <AI 2 type>' >&2
+    echo ''
+    echo 'Example: ./run.sh 2 1' >&2
+    echo ''
+    echo 'AI type:'
+    echo '    0: AITemplate' >&2
+    echo '    1: DummyAI' >&2
+    echo '    2: RandomAI' >&2
+    echo '    3: MinMaxAI' >&2
     exit -1
 fi
 
